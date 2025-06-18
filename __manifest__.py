5e93f8b2506432b3d5ec713c2151d168c1450c28
abdelmohsen22 <abdelmohsen.thabet@yahoo.com>
2025-06-18 16:12:50 +0300

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'HMS',
+    'summary': 'Hospitals Management System',
+    'author': 'Abdelmohsen Mohamed thabet',
+    'category': 'Services',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        'views/patient_views.xml',
+        'views/department_views.xml',
+        'views/doctor_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
+
